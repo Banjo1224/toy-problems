6aa8562500b952f30c6ef20db7411dcf41f76c00
Brandon Bates <bbates377@gmail.com>
2020-08-27 06:44:42 -0500

    finish primeTester

--- primeTester/primeTester.js
+++ primeTester/primeTester.js
@@ -7,6 +7,15 @@
 var primeTester = function(n) {
   if (typeof n !== 'number' || n < 1 || n % 1 !== 0) {
     // n isn't a number or n is less than 1 or n is not an integer
+    var divisors = [];
+    for (var i = 2; i < n; i++) {
+      if (n % i === 0) {
+        divisors.push(i);
+      }
+    }
+    if (divisors.length === 0) {
+      return false;
+    }
     return false;
   }
   // TODO: return true if n is prime, false otherwise
