acb32251a08786a3e8c8e01ac50d8f21ccfa675f
Brandon Bates <bbates377@gmail.com>
2020-08-26 19:32:38 -0500

    commonCharacters complete, proper commit this time

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -10,27 +10,43 @@
  * Extra credit: Extend your function to handle more than two input strings.
  */
 
+// TODO: Your code here!
+/*
+I: two strings
+O: one string of common characters
+C: No spaces, no repeats
+E: no common chars?
+*/
+// create object for storage
 
+// iterate through both strings
+
+// if a letter is not a key in obj, add key and set val to 1
+// if letter is already a key, add one to val
+// iterate through object keys
+// for every key with val > 1, concat to end of result string
+// return result
 
 var commonCharacters = function(string1, string2) {
-  // TODO: Your code here!
-  /*
-  I: two strings
-  O: one string of common characters
-  C: No spaces, no repeats
-  E: no common chars?
-*/
-  // create object for storage
   var store = {};
-  // iterate through both strings
-  var str = string1 + string2;
-  str.replaceAll("\\s", ""); // removes white spaces
-  console.log(str);
-  // if a letter is not a key in obj, add key and set val to 1
-  // if letter is already a key, add one to val
-  // iterate through object keys
-  // for every key with val > 1, concat to end of result string
-  // return result
+  var str = string1.concat(string2);
+  var result = "";
+  for (var i = 0; i < str.length; i++) {
+    var thisChar = str.charAt(i);
+    if (str.charAt(i) !== " ") {
+      if (store[str.charAt(i)]) {
+        store[str.charAt(i)]++;
+      } else {
+        store[str.charAt(i)] = 1;
+      }
+    }
+  }
+  for (var char in store){
+    if (store[char] > 1) {
+      result += char;
+    }
+  }
+  return result;
 };
 
-commonCharacters('aeiou ', 'laksjoi weoanlkv;alkjs');
+commonCharacters('aeiou ', 'laksjoi weoanlkvalkjs');
