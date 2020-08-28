8c2ce79a76dd4e15b61663082f59fd34e7790a2e
Brandon Bates <bbates377@gmail.com>
2020-08-28 18:57:57 -0500

    binary search working

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -11,5 +11,24 @@
  */
 
 var binarySearch = function (array, target) {
+  var search = function (start, end) {
+      var half = Math.floor((end - start) / 2 + start);
+      if (array[half] === target) {
+          return half;
+      }
+      if (array[half] > target) {
+          if (half === 0) return null;
+          return search(start, half);
+      }
+      if (array[half] < target) {
+          if (half === array.length-1) return null;
+          return search(half, end)
+      }
+  }
+  return search(0, array.length);
 };
 
+// var index = binarySearch([1, 2, 3, 4, 5], 4);
+// console.log(index); // 3
+// var index = binarySearch([1, 2, 3, 4, 5], 8);
+// console.log(index); // null
