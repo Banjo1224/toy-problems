876aeb65ef8a752cbd0c9d5a2e61cbccdfd1e158
Brandon Bates <bbates377@gmail.com>
2020-08-28 19:17:15 -0500

    characterFrequency working

--- characterFrequency/characterFrequency.js
+++ characterFrequency/characterFrequency.js
@@ -37,5 +37,47 @@
 
 
 var characterFrequency = function(string) {
+  var result = [];
+  var holding = {};
+  var input = string.split('');
+  for (var letter of input) {
+      if (holding[letter] === undefined) {
+          holding[letter] = 1;
+      } else holding[letter]++;
+  }
+  for (var letter in holding) {
+      var temp = [letter, holding[letter]];
+      result.push(temp);
+  }
   return result;
 };
+
+// console.log(characterFrequency('mississippi')) /*===
+//  *  [
+//  *    ['i', 4],
+//  *    ['s', 4],
+//  *    ['p', 2],
+//  *    ['m', 1]
+//  *  ]
+//  *
+//  *       :: Example2 ::
+//  */
+// console.log(characterFrequency('miaaiaaippi')) /*===
+//  *  [
+//  *    ['a', 4],
+//  *    ['i', 4],
+//  *    ['p', 2],
+//  *    ['m', 1]
+//  *  ]
+//  *
+//  *       :: Example3 ::
+//  */
+// console.log(characterFrequency('mmmaaaiiibbb'))
+// /*
+//  *  [
+//  *    ['a', 3],
+//  *    ['b', 3],
+//  *    ['i', 3],
+//  *    ['m', 3]
+//  *  ]
+//  */
