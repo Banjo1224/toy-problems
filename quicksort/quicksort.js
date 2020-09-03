d97b806938b362abdc048df6a87ae91424473a5b
Brandon Bates <bbates377@gmail.com>
2020-09-02 19:15:15 -0500

    quicksort working (i remember a lot of the sorting algorithms from high school)

--- quicksort/quicksort.js
+++ quicksort/quicksort.js
@@ -11,5 +11,29 @@
  */
 
 
-var quicksort = function(array) {
+var quicksort = function (array) {
+  //return original array if length is one
+  if (array.length <= 1) {
+    return array;
+  }
+  //declare pivot at last element
+  var pivot = array.pop();
+  var lesser = [];
+  var greater = [];
+  //iterate through array
+  for (var i = 0; i < array.length; i++) {
+    //if pivot is greater than array item, push to lesser
+    if (array[i] <= pivot) {
+      lesser.push(array[i]);
+      //if pivot is smaller, push to greater
+    } else {
+      greater.push(array[i]);
+    }
+  }
+  //recur through lesser and greater, return sorted array
+  return quicksort(lesser).concat(pivot, quicksort(greater));
 };
+
+//quicksort([8, 7, 6, 5, 4, 3, 2, 1]);
+//quicksort([6,4,3,5,7,2,3,4,7,3,2,57]);
+//quicksort([1]);
