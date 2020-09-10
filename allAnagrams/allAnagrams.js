fc6338c5ce2620fb28e783ead561859d42ca74d7
Brandon Bates <bbates377@gmail.com>
2020-09-10 11:53:04 -0500

    all Anagrams working recursively

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -12,6 +12,31 @@
   * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-var allAnagrams = function(string) {
-  // Your code here.
+ var allAnagrams = function(string) {
+  // declare result array
+  var result = {};
+  // recursive function to find unique strings
+  var anagram = (a, string) => {
+    // base case: if string is empty, push current anagram to result
+    if (string === '') {
+    result[a] = 1
+    }
+    // loop through string
+    for (var i = 0; i < string.length; i++) {
+      // run anagram on previous anagram plus one more letter from string
+        // also pass through the remaining letters in the string
+        // each run should take one letter out of word at a time
+      anagram(a + string[i], string.slice(0, i) + string.slice(i + 1));
+    }
+  }
+//   initial run for anagram function
+  anagram('', string);
+  // return result array
+  return Object.keys(result);
 };
+
+console.log(
+allAnagrams('abc'),
+allAnagrams('test'),
+allAnagrams('whoop')
+);
