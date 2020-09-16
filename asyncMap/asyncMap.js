44981ae5b822acf7690b8f4edfbda587e1b9b014
Brandon Bates <bbates377@gmail.com>
2020-09-15 19:48:30 -0500

    async map done

--- asyncMap/asyncMap.js
+++ asyncMap/asyncMap.js
@@ -37,6 +37,37 @@
  *
  */
 
-
 var asyncMap = function(tasks, callback) {
-};
+    var results = [];
+    var count = 0;
+    for (var i = 0; i < tasks.length; i++) {
+        (function(i) {
+            tasks[i]((val) => {
+                results[i] = val
+                count++;
+                if (count === tasks.length) {
+                    callback(results);
+                }
+                });
+        })(i);
+    }
+}
+
+
+
+asyncMap([
+  function(cb){
+    setTimeout(function(){
+    cb('one');
+    }, 200);
+  },
+  function(cb){
+    setTimeout(function(){
+      cb('two');
+  }, 100);
+  }],
+  function(results){
+    // the results array will equal ['one','two'] even though
+    // the second function had a shorter timeout.
+    console.log(results); // ['one', 'two']
+  });
