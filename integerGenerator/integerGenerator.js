26f04cedd69e12280b9ba660143feaa9dd66d34c
Brandon <bbates377@gmail.com>
2020-10-21 20:25:11 -0500

    integerGenerator

--- integerGenerator/integerGenerator.js
+++ integerGenerator/integerGenerator.js
@@ -4,13 +4,18 @@
  */
 
 // produces random integers in the range of [1,5] (inclusive)
-var random1to5 = function() {
+var random1to5 = function (toNum = 5) {
   // NOTE: You're allowed to use `Math.random()` in your solution.
   // TODO: you're code here!
+
+  return Math.floor(Math.random() * toNum) + 1;
 };
 
 // produces random integers in the range [1,7] (inclusive)
-var random1to7 = function() {
+var random1to7 = function () {
   // NOTE: Don't use `Math.random()` directly.
   // TODO:  your code here!
+  return random1to5(7);
 };
+
+console.log(random1to7())
